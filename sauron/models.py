f013a45455be91162ce75e7a76ff6103033119ae
Juan Funez <juan.funez@gmail.com>
2019-06-30 23:35:53 +0200

    first version of the working refactor

---
+++ sauron/models.py
@@ -0,0 +1,24 @@
+from typing import List, Dict, Any
+from pydantic import BaseModel, Json
+
+
+class JobModel(BaseModel):
+    job_type: str
+    name: str
+    args: Dict[str, Any] = None
+
+
+class JobsListModel(BaseModel):
+    jobs: List[JobModel]
+
+
+# class ConditionModel(JobModel):
+#     job_type: str = "condition"
+#     name: str
+#     args: Dict[str, Any] = None
+
+
+# class ActionModel(JobModel):
+#     job_type: str = "action"
+#     name: str
+#     args: Dict[str, Any] = None
