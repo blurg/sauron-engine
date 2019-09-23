a9c6e785231e7c990c65f1341280106e10aeb833
jlugao <joaolhullier@gmail.com>
2019-09-23 18:31:05 -0300

    Implemented Yaml tests

--- tests/engine_tests/parsers/test_default_parser.py
+++ tests/engine_tests/parsers/test_default_parser.py
@@ -39,3 +39,27 @@ class TestCaseOne:
         assert result[1].name == "print_the_equation"
         assert result[1].args == {"lower_number": 3, "greater_number": 10}
         assert result[1].job_type == "action"
+
+    def setup_yaml(self):
+        with open("tests/utils/first_rule.yaml","r") as f:
+            test_string = f.read().strip()
+        p = DefaultParser()
+        result = p.parse(test_string)
+        return result
+
+    def test_can_parse_the_test_yaml(self):
+        result = self.setup_yaml()
+
+        assert result is not None
+
+    def test_acuratelly_parsed_first_job_from_yaml(self):
+        result = self.setup_yaml()
+        assert result[0].name == "first_condition"
+        assert result[0].args == {"lower_number": 3, "greater_number": 10}
+        assert result[0].job_type == "condition"
+
+    def test_acuratelly_parsed_second_job_from_yaml(self):
+        result = self.setup_yaml()
+        assert result[1].name == "print_the_equation"
+        assert result[1].args == {"lower_number": 3, "greater_number": 10}
+        assert result[1].job_type == "action"
