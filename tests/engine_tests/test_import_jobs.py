131a15c68cc11f13456c6a79071fae1fcfb85d55
João Ricardo Lhullier Lugão <joaolhullier@gmail.com>
2019-10-30 18:47:07 -0300

    removes prints
    removes prints from test_import_jobs

--- tests/engine_tests/test_import_jobs.py
+++ tests/engine_tests/test_import_jobs.py
@@ -11,14 +11,12 @@ class TestFirstEngineCases:
     def test_can_parse_two_jobs(self):
         engine.import_jobs(job_import_module)
         jobs = engine.callables_collected
-        print(jobs)
         assert len(jobs) == 2
         assert [job for job in jobs.keys()] == ["always_true", "times_two"]
 
     def test_can_parse_jobs_with_verbose_names(self):
         engine.import_jobs(job_import_module_verbose)
         jobs = engine.callables_collected
-        print(jobs)
         assert len(jobs) == 2
         assert [job["verbose_name"] for job in jobs.values()] == [
             "True Always Is",
